0c4619af9bd99e1eaec679f6795ca983cd1a9e6c
rob <collinsduo1@gmail.com>
2023-09-05 16:21:09 -0500

    fix connection

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,30 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+
+// Load environment variables from a .env file if available
+dotenv.config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
+try {
+  if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+  } else {
+    sequelize = new Sequelize(
+      process.env.DB_NAME || 'your_default_db_name',
+      process.env.DB_USER || 'your_default_db_user',
+      process.env.DB_PASSWORD || 'your_default_db_password',
+      {
+        host: process.env.DB_HOST || 'localhost',
+        dialect: 'mysql',
+        port: process.env.DB_PORT || 3306
+      }
+    );
+  }
+} catch (error) {
+  console.error('Error connecting to the database:', error);
+  process.exit(1); // Terminate the application on a database connection error
 }
 
-module.exports = sequelize;
+module.exports = { sequelize };
+
--- server.js
+++ server.js
@@ -2,37 +2,58 @@ const express = require("express");
 const path = require("path");
 const controller = require("./controllers");
 const exphbs = require("express-handlebars");
-const sequelize = require("./config/connection");
 const session = require("express-session");
 const SequlizeStore = require("connect-session-sequelize")(session.Store);
 const helpers = require('./utils/helpers');
-const hbs = exphbs.create({ helpers });
+const { sequelize } = require('./config/connection');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
-const sess = {
-    secret: 'super secret secret',
-    cookie: {
-        maxAge: 60 * 60 * 1000,
-        httpOnly: true,
-        secure: false,
-        sameSite: 'strict',
-    },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequlizeStore({
-        db: sequelize
-    })
-};
 
-app.use(session(sess));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers'));
+(async () => {
+  try {
+    // Ensure the database connection is established
+    await sequelize.authenticate();
+    console.log('Database connection has been established successfully.');
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+    // Configure session
+    const sess = {
+        secret: 'super secret secret',
+        cookie: {
+            maxAge: 60 * 60 * 1000, // Session expires in 1 hour (adjust as needed)
+            httpOnly: true,
+            secure: false, // Set to 'true' if you use HTTPS
+            sameSite: 'strict',
+        },
+        resave: false,
+        saveUninitialized: true,
+        store: new SequlizeStore({
+            db: sequelize,
+        }),
+    };
 
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
-});
+    // Middleware
+    app.use(session(sess));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    // Use your controllers/routes
+    app.use(controller);
+
+    // Configure Handlebars as the view engine
+    const hbs = exphbs.create({ helpers });
+    app.engine('handlebars', hbs.engine);
+    app.set('view engine', 'handlebars');
+
+    // Start the server
+    sequelize.sync({ force: false }).then(() => {
+        app.listen(PORT, () => {
+            console.log(`Now listening on port ${PORT}`);
+        });
+    });
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+    process.exit(1); // Terminate the application on a database connection error
+  }
+})();
