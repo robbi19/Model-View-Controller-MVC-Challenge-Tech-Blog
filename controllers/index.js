dd1a42734c0c31d0dbc8a2c9231a2d28624c1429
rob <collinsduo1@gmail.com>
2023-09-06 15:22:39 -0500

    add config

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api/')
-const homeRoutes = require('./home-Routes.js');
-const dashboardRoutes = require('./dashboardRoutes.js');
+const homeRoutes = require('./homeRoutes.js');
+const dashRoutes = require('./dashRoutes.js');
 
 router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes);
+router.use('/dash', dashRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
